0a226a30ec3ed09fbf5fa90bda36c822cce66c1e
Git-iw <141666971+Git-iw@users.noreply.github.com>
2026-09-06 19:23:42 +0530

    Removed the Documents/JarvisAI folder

--- Documents/JarvisAI/Jarvis.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "C:\Users\cmanv\Documents\JarvisAI"
-call "C:\Users\cmanv\Documents\JarvisAI\.venv\Scripts\activate.bat"
-python main.py
-pause
--- Documents/JarvisAI/config.py
+++
@@ -1,6 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-GEMINI_API_KEY = os.getenv("AIzaSyBXU3td0VltM_9ykGQrqjHpbCvdnbP17kw")
